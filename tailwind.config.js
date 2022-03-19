6561c0f1ce5a2506e788dbb9f00724470cb3bbbd
Walid Berrouk <68415168+Walid-Berrouk@users.noreply.github.com>
2022-03-19 01:17:11 +0100

    Revert "countdown section"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    screens: {
-      mobile: { max: "750px" },
-      // => @media (max-width: 750px) { ... }
-    },
     extend: {},
   },
   plugins: [],
-};
+}
