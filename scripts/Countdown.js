bfe538de03dbe4e6ca29ac369a318fa244e1a67f
Walid Berrouk <jw_berrouk@esi.dz>
2022-03-29 21:24:49 +0100

    adding meta data

--- scripts/Countdown.js
+++ scripts/Countdown.js
@@ -3,7 +3,7 @@ const hours = document.getElementsByClassName("hoursTXT")[0];
 const minutes = document.getElementsByClassName("minutesTXT")[0];
 const seconds = document.getElementsByClassName("secondsTXT")[0];
 const currentyear = new Date().getFullYear();
-const newYearTime = new Date(`April 1 2022 08:00:00`);
+const newYearTime = new Date(`April 3 2022 17:00:00`);
 
 function updateCountdowntime() {
   const currentTime = new Date();
